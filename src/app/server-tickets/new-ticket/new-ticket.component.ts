d0e502942e523988ae031c367e9ed8024501587f
rb.balestrin <therealrenanbez@gmail.com>
2024-11-08 13:26:37 -0300

    feat: implement ticket management with new ticket creation, display, and status updates

--- src/app/server-tickets/new-ticket/new-ticket.component.ts
+++ src/app/server-tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,10 @@
-import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  output,
+  viewChild,
+  ViewChild,
+} from '@angular/core';
 import { ControlComponent } from '../../control/control.component';
 import { ButtonComponent } from '../../shared/button/button.component';
 import { FormsModule } from '@angular/forms';
@@ -12,8 +18,10 @@ import { FormsModule } from '@angular/forms';
 })
 export class NewTicketComponent {
   private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+  add = output<{ title: string; text: string }>();
 
   onSubmit(titleElement: string, descriptionElement: string) {
+    this.add.emit({ title: titleElement, text: descriptionElement });
     console.log(titleElement, descriptionElement);
 
     this.form().nativeElement.reset();
--- src/app/support-tickets/support-tickets.component.ts
+++ src/app/support-tickets/support-tickets.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
-import { DashboardItemComponent } from '../dashboard-item/dashboard-item.component';
 import { NewTicketComponent } from '../server-tickets/new-ticket/new-ticket.component';
+import { Ticket } from './ticket.model';
+import { TicketComponent } from '../ticket/ticket.component';
 
 @Component({
   selector: 'app-support-tickets',
   standalone: true,
-  imports: [DashboardItemComponent, NewTicketComponent],
+  imports: [NewTicketComponent, TicketComponent],
   templateUrl: './support-tickets.component.html',
   styleUrl: './support-tickets.component.css',
 })
-export class SupportTicketsComponent {}
+export class SupportTicketsComponent {
+  tickets: Ticket[] = [
+    {
+      id: '1',
+      title: 'Help with my account',
+      request: 'I need help logging in',
+      status: 'open',
+    },
+  ];
+
+  onAdd(ticketData: { title: string; text: string }) {
+    const ticket: Ticket = {
+      title: ticketData.title,
+      request: ticketData.text,
+      id: Math.random().toString(),
+      status: 'open',
+    };
+    this.tickets.push(ticket);
+  }
+  onClose(id: string) {
+    this.tickets = this.tickets.map((t) => {
+      if (t.id === id) {
+        return { ...t, status: 'closed' };
+      }
+      return t;
+    });
+  }
+}
