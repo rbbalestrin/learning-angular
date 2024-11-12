c2079c1f17665f6aaa1d1fea5d76e71c0e78f8a2
rb.balestrin <therealrenanbez@gmail.com>
2024-11-12 11:45:11 -0300

    feat: add Firebase authentication and Firestore configuration, implement admin check in app component

---
+++ src/app/app.config.ts
@@ -0,0 +1,26 @@
+export const Auth = new InjectionToken('Firebase auth', {
+  providedIn: 'root',
+  factory: () => {
+    const auth = getAuth();
+    if (environment.useEmulators) {
+      connectAuthEmulator(auth, 'http://localhost:9099', {
+        disableWarnings: true,
+      });
+    }
+    return auth;
+  },
+});
+
+export const FIRESTORE = new InjectionToken('Firestore', {
+  providedIn: 'root',
+  factory: () => {
+    let firestore: Firestore;
+    if (EnvironmentInjector.useEmulators) {
+      firestore = initializeFirestore(app, {});
+      connectFirestoreEmulator(firestore, 'localhost', 8080);
+    } else {
+      firestore = getFirestore();
+    }
+    return firestore;
+  },
+});
